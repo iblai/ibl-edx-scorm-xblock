93fdb537b54d936d2f13ae3fc0251121a271c7a9
Geoff Von Allmen <geoff@ibleducation.com>
2024-09-09 15:35:40 -0700

    refactor: add admin.py

---
+++ openedxscorm/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.ScormState)
+class ScormStateAdmin(admin.ModelAdmin):
+    list_display = ("id", "user" "course_key", "block_id")
+
+
+@admin.register(models.ScormInteraction)
+class ScormInteractionAdmin(admin.ModelAdmin):
+    list_display = ("id", "scorm_state", "interaction_id", "index")
